e0a66d7f139bc6632cf3f1797ff0c1206e689cbe
gilldunne <x00094469@ittd.ie>
2015-02-18 19:24:26 +0000

    InitialCommit Add missing init.py Tidy up code

--- dashboardIBM/db_wrapper.py
+++ dashboardIBM/db_wrapper.py
@@ -6,41 +6,15 @@ from datetime import timedelta, datetime
 client = MongoClient()
 db = client.TestEasyJLV
 
-#
-# This should only contain read / writes to the DB
-# Not currently set that way due to many migration errors
-#
-# class DBWrapper:
-#     client = MongoClient()
-#     db = client.TestEasyJLV
-#
-#     # Open a connection to TestEasyJLV
-#     def db_open(self):
-#         return self.db
-#
-#     # Close connection to TestEasyJLV
-#     def db_close(self):
-#         self.db_close()
-#
-#     # Use buildjob collection from TestEasyJLV
-#     def get_build_job_collection(self):
-#         return self.db.buildjob
-
 # Establishes a connection to the build job collection in the TestEasyJLV DB
 def get_buildjob_db_conn():
     collection = db.buildjob
     return collection
 
-# Gets the data to display the active and inactive servers and returns it to the view
-def get_active_inactive_build_jobs():
-    list_of_computer_names = get_computer_names()
-    list_of_build_jobs = get_build_jobs(list_of_computer_names)
-    end_dates_per_server = get_end_dates(list_of_computer_names, list_of_build_jobs)
-    graph_data = prepare_graph_data(list_of_computer_names, end_dates_per_server)
-    return graph_data
-
 # Loop through each build job on a server and get the last date it was built
-def get_end_dates(list_of_computer_names, list_of_build_jobs):
+def get_end_dates():
+    list_of_computer_names = get_computer_names()
+    list_of_build_jobs = get_build_jobs()
     collection = get_buildjob_db_conn()
     end_dates_per_server = []
     i = 0
@@ -50,46 +24,63 @@ def get_end_dates(list_of_computer_names, list_of_build_jobs):
             query = {"computerName": str(computer_name)}
             query.update({"name": str(job)})
             result = collection.find(query, {"endDate": 1}).sort("endDate", -1).limit(1)
-            # print >> sys.stderr, "Job: " + str(job)
-            # print >> sys.stderr, "End Date: " + str(result[0]["endDate"])
             list_of_dates.append(result[0]["endDate"])
         i += 1
-        # print >> sys.stderr, "* * * * * * * * * * * * * * * * * * * * * "
         end_dates_per_server.append(list_of_dates)
     return end_dates_per_server
 
 # Checks the date against todays date to see if server is active / inactive
-def prepare_graph_data(list_of_computer_names, end_dates_per_server):
-    graph_data = []
+def prepare_graph_data():
+    list_of_teams = get_list_teams()
+    end_dates_per_server = get_end_dates()
+    data = []
+
     last_seven_days = datetime.today() - timedelta(days=7)
     i = 0
-    for computer_name in list_of_computer_names:
+    for team_name in list_of_teams:
         list_data = []
+        graph_data = []
         for date_of_build_job in end_dates_per_server[i]:
             if date_of_build_job >= last_seven_days:
                 list_data.append(True)
             else:
                 list_data.append(False)
         i += 1
-        graph_data.append(computer_name)
+        graph_data.append(team_name)
         graph_data.append(list_data)
-    return graph_data
+        data.append(graph_data)
+    return data
 
-# Name of the Servers
+# Name of the Servers - Domain Names
 def get_computer_names():
     collection = get_buildjob_db_conn()
     list_of_computer_names = collection.distinct("computerName")
     return list_of_computer_names
 
-# Gets the number of all the buildjob per team and returns to the view
-def get_all_buildjob():
-    list_of_computer_names = get_computer_names()
-    list_of_build_jobs = get_build_jobs(list_of_computer_names)
-    total_number_buildjob = get_total_number_buildjob(list_of_computer_names, list_of_build_jobs)
-    return total_number_buildjob
+
+def get_list_teams():
+    collection = get_buildjob_db_conn()
+    list_of_team_names = collection.distinct("teamName")
+    return list_of_team_names
+
+
+def get_build_job_per_team():
+    collection = get_buildjob_db_conn()
+    list_of_teams = get_list_teams()
+    team_list = []
+    for team in list_of_teams:
+        buildjob_list = []
+        query = collection.find({"teamName": str(team)}).distinct("name")
+        x = len(query)
+        buildjob_list.append(team)
+        # buildjob_list.append(query)
+        buildjob_list.append(x)
+        team_list.append(buildjob_list)
+    return team_list
 
 # Loop through the computer names and get each build job name
-def get_build_jobs(list_of_computer_names):
+def get_build_jobs():
+    list_of_computer_names = get_computer_names()
     collection = get_buildjob_db_conn()
     answer = []
     for computer_name in list_of_computer_names:
@@ -99,23 +90,6 @@ def get_build_jobs(list_of_computer_names):
             list_of_build_jobs.append(str(job))
         answer.append(list_of_build_jobs)
     return answer
-
-# Loop through the computers and get the number of buildjobs in that domain
-def get_total_number_buildjob(list_of_computer_names, list_of_build_jobs):
-    collection = get_buildjob_db_conn()
-    buildjob_list = []
-    for computer_name in list_of_computer_names:
-        query = collection.find({"computerName": str(computer_name)}).distinct("name")
-        x = len(query)
-        buildjob_list.append(computer_name)
-        buildjob_list.append(query)
-        buildjob_list.append(x)
-    return buildjob_list
-
-# TODO - Get number active servers for the graph
-def get_number_active_servers():
-
-    return None
 
 # Get the user and the servers they are connected to
 def get_user_data(list_of_computer_names):
--- dashboardIBM/views/views.py
+++ dashboardIBM/views/views.py
@@ -6,22 +6,40 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
+import sys
 from dashboardIBM import db_wrapper
 
-
+# TODO - Need to get the figures correct here
+# Should show the active and inactive numbers
+# only shows the active currently
 @api_view(['GET', ])
 def get_active_inactive_build_jobs(request):
     """
     <!--  HTML goes here -->
     """
-    data = db_wrapper.get_active_inactive_build_jobs()
-
-    return Response(json.loads(dumps(data)))
+    test = []
+    act = ""
+    data = db_wrapper.prepare_graph_data()
+    # print >> sys.stderr, type(data)
+    for i in data:
+        print >> sys.stderr, i
+        amt = 0
+        for j in i:
+            if(j == True):
+                amt += 1
+                act = "active"
+            elif(j == False):
+                amt += 1
+                act = "inactive"
+        test.append(act)
+        test.append(amt)
+    # json.loads(dumps(data))
+    return Response(data)
 
 
 @api_view(['GET', ])
 def get_all_buildjob(request):
-    data = db_wrapper.get_all_buildjob()
+    data = db_wrapper.get_build_job_per_team()
     return Response(json.loads(dumps(data)))
 
 
